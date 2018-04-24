e0d347c5955f1ed1e9666537e2c0bc0d2e55faa2
samuraiballhiro <yuy.hirohiro@gamil.com>
2018-04-24 20:03:25 +0900

    chenge 'hello world' to 'hello spring world'

--- hajiboot/src/main/java/com/example/hajiboot/HajibootApplication.java
+++ hajiboot/src/main/java/com/example/hajiboot/HajibootApplication.java
@@ -2,11 +2,21 @@ package com.example.hajiboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HajibootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HajibootApplication.class, args);
-	}
+
+    @GetMapping("/")
+    String home(){
+        return "Hello spring world";
+    }
+
+    public static void main(String[] args){
+        SpringApplication.run(HajibootApplication.class,args);
+    }
+
 }
