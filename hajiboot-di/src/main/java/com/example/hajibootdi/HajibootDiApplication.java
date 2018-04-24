efa016c34d27aed0d592b2a1ab71f49a50976e51
samuraiballhiro <yuy.hirohiro@gamil.com>
2018-04-24 20:08:22 +0900

    add hajiboot-d

---
+++ hajiboot-di/src/main/java/com/example/hajibootdi/HajibootDiApplication.java
@@ -0,0 +1,12 @@
+package com.example.hajibootdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HajibootDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HajibootDiApplication.class, args);
+	}
+}
