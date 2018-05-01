20f199efe9e85911ae823c026b96ad208478c037
samuraiballhiro <yuy.hirohiro@gamil.com>
2018-05-01 17:55:40 +0900

    flywayを追加

--- hajiboot-thymeleaf/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO customers(first_name, last_name) VALUES ('Yuya', 'Hirooka');
-INSERT INTO customers(first_name, last_name) VALUES ('Yuya', 'Hirooka');
-INSERT INTO customers(first_name, last_name) VALUES ('Yuya', 'Hirooka');
-INSERT INTO customers(first_name, last_name) VALUES ('Yuya', 'Hirooka');
