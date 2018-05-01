e6d08816898fce287c03ec0152f5fcb148e1199d
samuraiballhiro <yuy.hirohiro@gamil.com>
2018-05-01 15:07:15 +0900

    adding bootstarap

---
+++ hajiboot-thymeleaf/src/main/java/com/example/domain/Customer.java
@@ -0,0 +1,18 @@
+package com.example.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer implements Serializable {
+
+    private Integer id;
+    private String firstName;
+    private String lastName;
+
+}
---
+++ hajiboot-thymeleaf/src/main/java/com/example/web/CustomerController.java
@@ -0,0 +1,77 @@
+package com.example.web;
+
+import com.example.domain.Customer;
+import com.example.service.CustomerService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("customers")
+public class CustomerController {
+
+    @Autowired
+    CustomerService customerService;
+
+    //Formの初期化、@RequestMappingでマッピングされたメソッドの前に実行され
+    //返り値は自動でモデルに追加される。
+    @ModelAttribute
+    CustomerForm setUpForm() {
+        return new CustomerForm();
+    }
+
+    @GetMapping
+    String list(Model model) {
+        List<Customer> customers = customerService.findAll();
+        model.addAttribute("customers", customers);
+        return "customers/list";
+    }
+
+    @PostMapping(path = "create")
+        //Validatedアノテーションが評価された後に隣のBindingResultに結果が格納される。
+    String create(@Validated CustomerForm form, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return list(model);
+        }
+        Customer customer = new Customer();
+
+        //フィールド名が同じものの場合につかえるBean変換
+        //より柔軟なものにはDozerやModelMapperが存在する。
+        BeanUtils.copyProperties(form, customer);
+        customerService.create(customer);
+        return "redirect:/customers";
+    }
+
+    @GetMapping(path = "edit", params = "form")
+    String edit(@RequestParam Integer id, CustomerForm form ) {
+        Customer customer = customerService.findOne(id);
+        BeanUtils.copyProperties(customer,form);
+        return "customers/edit";
+    }
+
+    @PostMapping(path = "edit")
+    String editForm(@RequestParam Integer id, @Validated CustomerForm form){
+        Customer customer = customerService.findOne(id);
+        BeanUtils.copyProperties(form,customer);
+        customer.setId(id);
+        customerService.update(customer);
+        return "redirect:/customers";
+    }
+
+    @PostMapping(path="edit", params = "goToTop")
+    String goToTop(){
+        return "redirect:/customers";
+    }
+
+    @PostMapping(path = "delete")
+    String delete(@RequestParam Integer id){
+        customerService.delete(id);
+        return "redirect:/customers";
+    }
+}
